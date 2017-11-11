f36ac5683f171e02b966e296bbdbd2ae1d190e51
Shipu Ahamed <shipuahamed01@gmail.com>
2017-11-12 03:41:27 +0600

    Adding Config file

--- config/sslwpayment.php
+++ config/sslwpayment.php
@@ -1,7 +1,18 @@
 <?php
 
 return [
-    'sid' => '',
-    'user' => '',
-    'password'=> ''
+    'store_id' => '',
+    'store_password' => '',
+    'sandbox' => true,
+    'redirect_url' => [
+        'fail' => [
+            'route' => '' // payment.failed
+        ],
+        'success' => [
+            'url' => '' //payment.success
+        ],
+        'cancel' => [
+            'url' => '' // payment/cancel or you can use route also
+        ]
+    ]
 ];
