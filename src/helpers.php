12123e8653de4541186a741810ac825474854d16
Hashemi Rafsan <rafsanhashemi@deligram.com>
2020-03-05 12:50:44 +0600

    fix indentation

--- src/helpers.php
+++ src/helpers.php
@@ -2,8 +2,7 @@
 use \Shipu\SslWPayment\Payment;
 
 if (! function_exists('ssl_wireless_post_button')) {
-    function ssl_wireless_post_button( $input, $amount, $buttonText = 'Payment', $class = '' )
-    {
+    function ssl_wireless_post_button( $input, $amount, $buttonText = 'Payment', $class = '' ) {
         return '<form action="'.ssl_wireless_payment_url().'" method="POST" style="display:inline">' .
             ssl_wireless_hidden_input($input, $amount) .
             '<button type="submit" class="'.$class.'">'.$buttonText.'</button>' .
